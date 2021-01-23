f382c1f98efbb838fb0130707a1e3ba5cd07397f
soyoo <soyoo@aliyun.com>
2021-01-23 22:58:10 +0800

    add logrus

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module octopus
 
 go 1.15
 
-require github.com/tidwall/evio v1.0.8 // indirect
+require (
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/tidwall/evio v1.0.8 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/evio"
 )
 
 func main() {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
+
 	var events evio.Events
 	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
 		fmt.Println(len(in))
